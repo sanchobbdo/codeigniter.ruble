86e0a9de06f52d2a2de43990ac3ad797e0996262
Camilo Aguilar <camilo.aguilar.r@gmail.com>
2012-07-29 18:09:00 -0500

    oops, mistaken repo

--- templates/project_templates.rb
+++ templates/project_templates.rb
@@ -1,6 +1,6 @@
 project_template "CodeIgniter Template" do |t|
 	t.type = :php
-	t.location = "https://github.com/NucleoDigital/codeigniter.ruble.git"
+	t.location = "https://github.com/NucleoDigital/CodeIgniter.git"
 	t.description = "Code Igniter template"
 	t.replace_parameters = false
 end
